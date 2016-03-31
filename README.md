a2005b4e310bad0d9177febf4da435795df13fb3
Rub21 <ruben@mapbox.com>
2016-03-31 10:17:24 -0500

    files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# update-tofix-task
+# update-tofix
+
+Use this app to update [to-fix](http://osmlab.github.io/to-fix/) tasks.
+
+### Comand
+
+```
+node index.js --idtask=idetofixtask --host=host --csvfile=path/to/csvfile/ --password=xxx
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var request = require("request");
+var argv = require('minimist')(process.argv.slice(2));
+var host = argv.host;
+var csvfile = argv.csvfile;
+var idtask = argv.idtask;
+var password = argv.password;
+request({
+  url: host + '/detail/' + idtask,
+  json: true
+}, function(err, response, task) {
+  if (!err && response.statusCode === 200) {
+    var formData = {
+      id: task.id,
+      name: task.title,
+      source: task.source,
+      description: task.description,
+      changeset_comment: task.changeset_comment,
+      password: password,
+      file: fs.createReadStream(csvfile),
+      preserve: 'true',
+      newtask: 'false'
+    };
+    // update task
+    request.post({
+      url: host + '/csv',
+      formData: formData
+    }, function optionalCallback(err, res, body) {
+      if (err) {
+        console.log(err);
+      }
+      console.log('Successful Update!' + body);
+    });
+  } else {
+    console.log('the task doesnt exit');
+  }
+});
